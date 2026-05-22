16fdff25dea3dac6ebf553ce960b710ae98ed1ee
Andrew Wong <wongwong.wong@gmail.com>
2026-05-22 10:15:08 +0900

    wip: actually hit the OneFrame api with the client
    calling the proxy endpoint now prints:
    ```
    Right(http://localhost:8080/rates?pair=USDJPY)
    {"error":"Forbidden"}
    ```
    
    TODO: add token header

--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -1,13 +1,17 @@
 package forex.services.rates
 
 import cats.Applicative
+import cats.effect.Sync
 import forex.config.OneFrameConfig
 import interpreters._
 import org.http4s.client.Client
 
 object Interpreters {
   def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F]()
-  def live[F[_]: Applicative](client: Client[F], config: OneFrameConfig): Algebra[F] =
+  // F[_] => F is a type constructor that takes one *type* argument, e.g. IO, not String
+  // : Sync => F "implements" Sync, i.e. F has a Sync instance--Sync[F], e.g. Sync[IO]
+  //   (Sync[IO] defined in the cats-effect IO companion object)
+  def live[F[_]: Sync](client: Client[F], config: OneFrameConfig): Algebra[F] =
     new OneFrameLive[F](client, config)
 
 }
--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameLive.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameLive.scala
@@ -1,19 +1,33 @@
 package forex.services.rates.interpreters
 
 import forex.services.rates.Algebra
-import cats.Applicative
+import cats.effect.Sync
 import cats.syntax.applicative._
+import cats.syntax.apply._
 import cats.syntax.either._
+import cats.syntax.functor._
 
 import forex.config.OneFrameConfig
 import forex.domain.{ Price, Rate, Timestamp }
 import forex.services.rates.errors._
 import org.http4s.client.Client
-//import org.http4s.headers._
-//import org.http4s.{ Method, Uri }
-import org.http4s.{ Uri  }
+import org.http4s.Uri
 
-class OneFrameLive[F[_]: Applicative](@annotation.unused client: Client[F], @annotation.unused config: OneFrameConfig) extends Algebra[F] {
+/*
+ NOTES: http4s
+ def expect[A](uri: Uri)(implicit d: EntityDecoder[F, A]): F[A]
+ - implicit means compiler searches for a matching type
+ - implicit String decoder exists in EntityDecoder companion object
+
+ http4s's EntityDecoder[F, String] (the text decoder) requires Sync
+ (decoding the body involves draining the response stream,
+ which is a side effect that must be suspended, which only Sync can do)
+ ("suspending a side effect" means wrapping it,
+ so it doesn't execute until outside pure code, i.e. IOApp.stream)
+
+ btw, cats is separate from cats-effect
+ */
+class OneFrameLive[F[_]: Sync](client: Client[F], @annotation.unused config: OneFrameConfig) extends Algebra[F] {
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
     // there is cats "either" syntax to make this prettier
     val target: Either[Error, Uri] = Uri.fromString(s"http://${config.host}:${config.port}") match {
@@ -23,20 +37,17 @@ class OneFrameLive[F[_]: Applicative](@annotation.unused client: Client[F], @ann
       case Left(e) =>
         Left(Error.OneFrameLookupFailed(e.getMessage))
     }
-    println(target)
-/*
-    val req = Method.GET(
-      target,
-      Authorization(Credentials.Token(AuthScheme.Bearer, config.token)),
-    )
-*/
 
+    println(target)
+
+    // .map is from cats.syntax.functor (more generic than scala map)
+    // `*>` is from cats.syntax.apply (a.k.a. "andThen")
     target match {
-      case Right(_) =>
-        Rate(pair, Price(BigDecimal(1337)), Timestamp.now).asRight[Error].pure[F]
+      case Right(uri) =>
+        client.expect[String](uri).map(println) *>
+          Rate(pair, Price(BigDecimal(1337)), Timestamp.now).asRight[Error].pure[F]
       case Left(e) =>
         e.asLeft[Rate].pure[F]
     }
   }
-
 }
